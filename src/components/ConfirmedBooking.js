d7df8b28ae56903bf5bbcdcb83f263dbce32dc76
zac nation <zac.nation@icloud.com>
2023-10-09 18:15:03 +0100

    Initial commit

---
+++ src/components/ConfirmedBooking.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const ConfirmedBooking = () => {
+  return (
+    <div className="comfirm">
+      <div>
+        <h1>Booking has been confirmed!</h1>
+      </div>
+    </div>
+  );
+};
+
+export default ConfirmedBooking;
---
+++ src/recipes.js
@@ -0,0 +1,28 @@
+const recipes = [
+  {
+    id: 1,
+    title: 'Greek salad',
+    price: 12.99,
+    image: 'https://i.ibb.co/68c9bLv/greek-salad.jpg',
+    description:
+      'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Deleniti esse ut reprehenderit deserunt illo officiis itaque quasi doloribus, veritatis sint! Exercitationem.',
+  },
+  {
+    id: 2,
+    title: 'Bruchetta',
+    price: 5.99,
+    image: 'https://i.ibb.co/317GqZv/Bruchetta.png',
+    description:
+      'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Deleniti esse ut reprehenderit deserunt illo officiis itaque quasi doloribus, veritatis sint! Exercitationem.',
+  },
+  {
+    id: 3,
+    title: 'Lemon Dessert',
+    price: 4.78,
+    image: 'https://i.ibb.co/sm6MfcH/desert.jpg',
+    description:
+      'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Deleniti esse ut reprehenderit deserunt illo officiis itaque quasi doloribus, veritatis sint! Exercitationem.',
+  },
+];
+
+export default recipes;
